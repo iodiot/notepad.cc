6808a10d1886dc663b7d8c1f7c0e294be483fcec
io <iodiot@gmail.com>
2011-01-26 15:39:36 +0200

    ass

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,10 +30,6 @@ class User < ActiveRecord::Base
     nil
   end
 
-  def updated_ago
-    time_ago(self.updated_at)
-  end
-
   #
   def self.random_login
     login = ''
@@ -63,24 +59,6 @@ class User < ActiveRecord::Base
 
   def self.encrypt(passwd, salt)
     Digest::SHA2.hexdigest(passwd + salt)
-  end
-
-  def time_ago(from_time, to_time = Time.now, include_seconds = false, detail = false)
-    from_time = from_time.to_time if from_time.respond_to?(:to_time)
-    to_time = to_time.to_time if to_time.respond_to?(:to_time)
-    distance_in_minutes = (((to_time - from_time).abs)/60).round
-    distance_in_seconds = ((to_time - from_time).abs).round
-    case distance_in_minutes
-    when 0..1           then time = (distance_in_seconds < 60) ? "#{distance_in_seconds} seconds ago" : '1 minute ago'
-      when 2..59          then time = "#{distance_in_minutes} minutes ago"
-      when 60..90         then time = "1 hour ago"
-      when 90..1440       then time = "#{(distance_in_minutes.to_f / 60.0).round} hours ago"
-      when 1440..2160     then time = '1 day ago' # 1-1.5 days
-      when 2160..2880     then time = "#{(distance_in_minutes.to_f / 1440.0).round} days ago" # 1.5-2 days
-      else time = from_time.strftime("%a, %d %b %Y")
-    end
-    return time_stamp(from_time) if (detail && distance_in_minutes > 2880)
-    return time
   end
 end
 
